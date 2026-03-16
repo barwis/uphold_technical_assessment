a8a3d3efe63bace10f60b706f48cb20d5bee951b
barwis <wisniewski.bart@gmail.com>
2026-03-16 09:03:20 +0000

    Deploy React app to GitHub Pages

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', {
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_',
+    }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-no-target-blank': 'error',
+    'react/self-closing-comp': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': ['error', {
+      'groups': [
+        'builtin',
+        'external',
+        'internal',
+        'parent',
+        'sibling',
+        'index'
+      ],
+      'newlines-between': 'never',
+      'alphabetize': {
+        'order': 'asc',
+        'caseInsensitive': true
+      }
+    }]
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'error',
+      }
+    },
+    {
+      files: ['**/*.test.ts', '**/*.test.tsx'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'error',
+      }
+    },
+    {
+      files: ['**/test-polyfills.ts'],
+      rules: {
+        'no-var': 'off',
+      }
+    }
+  ]
+};
