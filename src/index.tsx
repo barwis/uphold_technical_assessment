39ed7268b0642fbc21f4564e5a681589f9dab6f1
barwis <wisniewski.bart@gmail.com>
2026-03-16 09:27:54 +0000

    Re-enable msw for non-development envs. set relative path to msw file

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,12 @@ import { ErrorBoundary } from "./components";
 import reportWebVitals from "./reportWebVitals";
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
-
   const { worker } = await import("./mocks/browser");
 
   return worker.start({
+    serviceWorker: {
+      url: "./mockServiceWorker.js",
+    },
     onUnhandledRequest: "bypass",
   });
 }
